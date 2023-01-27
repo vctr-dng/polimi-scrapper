f9b817d5acf9e3d82de60abc1f098f46ad23965c
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2023-01-27 23:30:27 +0100

    scrapper can print the dataframe result

--- scrap-polimi.py
+++ scrap-polimi.py
@@ -1,18 +1,43 @@
 import argparse
-
+import multiprocessing
+from multiprocessing.pool import ThreadPool as Pool
 from os import path
+
+import pandas as pd
+from tqdm import tqdm
 
 from scrapper import Scrapper
 
+
+def scrapURLs(URLs):
+
+    webScrapper = Scrapper()
+
+    df = pd.DataFrame()
+
+    pool = Pool(multiprocessing.cpu_count() * 2)
+
+    for relevantInformation in tqdm(
+        pool.imap_unordered(webScrapper.getRelevantInformation, URLs),
+        total=len(URLs),
+        colour="green",
+    ):
+        df = pd.concat([relevantInformation, df], ignore_index=True)
+
+    return df
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description = """Gather informations regarding Politecnico di Milano courses details page
+        description="""Gather informations regarding Politecnico di Milano courses details page
         """
     )
 
-    parser.add_argument('input', help='String of an URL or path to a file containing URLs')
-    parser.add_argument('output', help='Path to save the tsv file output')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument(
+        "input", help="String of an URL or path to a file containing URLs"
+    )
+    parser.add_argument("output", help="Path to save the tsv file output")
+    parser.add_argument("-v", "--verbose", action="store_true")
 
     args = parser.parse_args()
     verbose = args.verbose
@@ -22,6 +47,20 @@ if __name__ == "__main__":
     isOutputDir = True if path.isdir(args.output) else False
 
     if not isOutputDir and not path.isfile(args.output):
-        raise Exception(f'The specified output : {args.output} is not pointing to a directory nor a file')
+        raise Exception(
+            f"The specified output : {args.output} is not pointing to a directory nor a file"
+        )
 
-    scrap = Scrapper()
+    URLs = []
+    if isInputFile:
+        inputFile = open(args.input, "r")
+        data = inputFile.read()
+        inputFile.close()
+        URLs = data.split("\n")
+    else:
+        URLs.append(args.input)
+
+    if verbose:
+        print(f'{len(URLs)} link{"s" if len(URLs) > 1 else ""} will be scrapped')
+
+    print(scrapURLs(URLs))
--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,20 @@
-class Scrapper():
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scrapper:
+
+    labels = [
+        "ID Code",
+        "Course Title",
+        "Credits (CFU / ECTS)",
+        "Name",
+        "Track",
+        "Semester",
+    ]
+
+    renamed_labels = {"Name": "Master", "Credits (CFU / ECTS)": "Credits"}
 
     def __init__(self):
         pass
@@ -6,3 +22,42 @@ class Scrapper():
     @staticmethod
     def __call__():
         print("called")
+
+    @staticmethod
+    def getURLContent(URL: str):
+        request = requests.get(URL, stream=True)
+        return request.content
+
+    @staticmethod
+    def getContentEIC(EIC):
+        return " ".join(EIC.get_text().split())
+
+    @staticmethod
+    def getRelevantInformation(URL: str) -> pd.DataFrame:
+
+        soup = BeautifulSoup(Scrapper.getURLContent(URL), "html.parser")
+
+        mainSection = soup.find_all("td", class_="CenterBar")[0]
+
+        InfoBoxes = mainSection.find_all("table", class_="BoxInfoCard")
+
+        data = {}
+
+        for infoBox in InfoBoxes:
+            labelEIC = infoBox.find_all("td", class_="ElementInfoCard1")
+            valueEIC = infoBox.find_all("td", class_="ElementInfoCard2")
+
+            for i in range(len(labelEIC)):
+                label = Scrapper.getContentEIC(labelEIC[i])
+                value = Scrapper.getContentEIC(valueEIC[i])
+                if label == "Credits (CFU / ECTS)":
+                    data[label] = float(value)
+                else:
+                    data[label] = str(value)
+
+        relevantData = pd.DataFrame(
+            {label: data[label] for label in Scrapper.labels}, index=[0]
+        )
+        relevantData.insert(relevantData.shape[1], "URL", URL)
+
+        return relevantData
